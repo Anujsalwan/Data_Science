a4ecbe452e96221b067eea3c2abc4fbb0407efcd
CA_Anuj_salwan <caanujsalwan@gmail.com>
2026-05-04 17:27:36 +0530

    Fixes

--- 001_Courses_Projects/ML_Projects/Supervised_ML/Life_Expectancy/app_ML_Income.py
+++ 001_Courses_Projects/ML_Projects/Supervised_ML/Life_Expectancy/app_ML_Income.py
@@ -39,7 +39,7 @@ st.caption(
 # 2. Load model + preprocessing artifacts (cached)
 # ---------------------------------------------------------------------------
 MODEL_CANDIDATES = [
-    "best_xgboost_classifier.pkl",
+    "best_gradient_boosting_model.pkl",
 ]
 
 
